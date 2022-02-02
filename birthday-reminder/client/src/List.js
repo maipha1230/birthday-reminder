8e5ef62626ad9798f9b66da4278b8515a76d1ef5
maipha1230 <maipha1230@gmail.com>
2022-02-02 10:15:35 +0700

    Update List.js

--- birthday-reminder/client/src/List.js
+++ birthday-reminder/client/src/List.js
@@ -1,78 +1,78 @@
-import React, { useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserMinus, faUserEdit } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUserMinus,
+  faUserEdit,
+  faBirthdayCake,
+} from "@fortawesome/free-solid-svg-icons";
 import Update from "./Update";
-import Add from './add'
+import Add from "./add";
 import axios from "axios";
 
 const List = () => {
-
   const [people, setPeople] = useState([]);
 
   const [updateModal, setUpdateModal] = useState(false);
-  const [ updateId, setUpdateId ] = useState()
-  const [ updateName, setUpdateName ] = useState('')
-  const [ updateBday, setUpdateBday ] = useState(null)
-  const [ updatePic, setUpdataePic ] = useState('')
+  const [updateId, setUpdateId] = useState();
+  const [updateName, setUpdateName] = useState("");
+  const [updateBday, setUpdateBday] = useState(null);
+  const [updatePic, setUpdataePic] = useState("");
   const [addModal, setAddModal] = useState(false);
-  
-  
+
   const toggleAddModal = () => {
     setAddModal(true);
   };
 
+  useEffect(() => {
+    //use for fetch birthday when page render
+    fetchPeople();
+  }, []);
 
-  
-  
-  useEffect(() => { //use for fetch birthday when page render
-    fetchPeople()
-  },[])
-
-
-  const fetchPeople = async () => {// fetch birthdays list from database
-    await axios.get('http://localhost:3001/')
-    .then((response) => {
-      setPeople(response.data)
-      console.log(response.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-  }
-
- 
+  const fetchPeople = async () => {
+    // fetch birthdays list from database
+    await axios
+      .get("http://localhost:3001/")
+      .then((response) => {
+        setPeople(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   //UpdateModal toggle
   const toggleUpdateModal = (id, name, bday, pic) => {
     setUpdateModal(true);
-    setUpdateId(id)
-    setUpdateName(name)
-    setUpdateBday(bday)
-    setUpdataePic(pic)
+    setUpdateId(id);
+    setUpdateName(name);
+    setUpdateBday(bday);
+    setUpdataePic(pic);
   };
 
   //delete Bday person from list
   const deletePeople = (id) => {
-    const delId = id
-    axios.delete(`http://localhost:3001/delete/${delId}`)
-    .then((response) => {
+    const delId = id;
+    axios.delete(`http://localhost:3001/delete/${delId}`).then((response) => {
       setPeople(
         people.filter((val) => {
-        return val.id !== id
-      }))
+          return val.id !== id;
+        })
+      );
+    });
+  };
 
-    })
-  }
-
-  const clearAll = async () => {// clear all bday from list
-    await axios.delete('http://localhost:3001/clear')
-    .then((response) => {
-      setPeople([])
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-  }
+  const clearAll = async () => {
+    // clear all bday from list
+    await axios
+      .delete("http://localhost:3001/clear")
+      .then((response) => {
+        setPeople([]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const btnDel = {
     backGroundColor: "red",
@@ -92,23 +92,49 @@ const List = () => {
   };
 
   return (
-
     <>
-        <section className="container">
-
-    <h3> {people.length} in birthdays list</h3>
-      {people.map((person) => {
-        const { id, name, Bday, pic } = person;
-        const today = new Date().getFullYear();
-        const bday = new Date(Bday);
-        const age = today - bday.getFullYear();
-        return (  
+      <section className="container">
+        <h3> {people.length} in birthdays list</h3>
+        {people.map((person) => {
+          const { id, name, Bday, pic } = person;
+          const year = new Date().getFullYear();
+          const today = new Date().getDate()
+          const bday = new Date(Bday);
+          const age = year - bday.getFullYear();
+          const birthdayCheck = today - bday.getDate() === 0
+          return (
             <article key={id} className="person">
               <img src={pic} alt={name} />
               <div style={{ position: "relative" }}>
-                <button style={btnDel} className="delete">
+                {birthdayCheck && 
+                  <button
+                    style={{
+                      width: "24px",
+                      height: "24px",
+                      position: "absolute",
+                      top: "-40px",
+                      right: 58,
+                      cursor: 'auto'
+                    }}
+                  >
+                    <FontAwesomeIcon
+                      icon={faBirthdayCake}
+                      style={{
+                        width: 16,
+                        height: 16,
+                        position: "absolute",
+                        top: 0,
+                        right: 2,
+                      }}
+                    ></FontAwesomeIcon>
+                  </button>
+                }
+                <button
+                  style={btnDel}
+                  className="delete"
+                  onClick={() => deletePeople(id)}
+                >
                   <FontAwesomeIcon
-                    onClick={() => deletePeople(id)}
                     style={{
                       width: 16,
                       height: 16,
@@ -119,9 +145,12 @@ const List = () => {
                     icon={faUserMinus}
                   ></FontAwesomeIcon>
                 </button>
-                <button style={btnUpdate} className="update">
+                <button
+                  style={btnUpdate}
+                  className="update"
+                  onClick={() => toggleUpdateModal(id, name, bday, pic)}
+                >
                   <FontAwesomeIcon
-                    onClick={() => toggleUpdateModal(id, name, bday, pic)}
                     style={{
                       width: 16,
                       height: 16,
@@ -134,16 +163,24 @@ const List = () => {
                 </button>
                 <h4>{name}</h4>
                 <p>{age} years</p>
-              </div> 
-            </article>              
-        );
-      })}        
-      <button onClick={() => toggleAddModal()}>Add birthday</button>
-      <button onClick={() => clearAll()}>Clear all</button>
+              </div>
+            </article>
+          );
+        })}
+        <button onClick={() => toggleAddModal()}>Add birthday</button>
+        <button onClick={() => clearAll()}>Clear all</button>
 
-      { addModal && <Add closeModal={setAddModal} />}
-      
-      { updateModal && <Update closeModal={setUpdateModal}  id={updateId} name={updateName} bday={updateBday} pic={updatePic} /> }
+        {addModal && <Add closeModal={setAddModal} />}
+
+        {updateModal && (
+          <Update
+            closeModal={setUpdateModal}
+            id={updateId}
+            name={updateName}
+            bday={updateBday}
+            pic={updatePic}
+          />
+        )}
       </section>
     </>
   );
